4b8094e1d7319ee9419acaca607ed683711859db
Tiago Dionizio <tiagosdionizio@gmail.com>
2018-09-15 23:37:34 -0300

    Scripts de limpeza e de teste

---
+++ limpar.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -f 1080p_Robert.PGM
+rm -f 720p_Robert.PGM
+rm -f 480p_Robert.PGM
+rm -f 360p_Robert.PGM
+
+rm -f 1080p_Realce.PGM
+rm -f 720p_Realce.PGM
+rm -f 480p_Realce.PGM
+rm -f 360p_Realce.PGM
---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./image 1080p.PGM
+./image 720p.PGM
+./image 480p.PGM
+./image 360p.PGM
